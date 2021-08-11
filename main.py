d8ce3b95a11429f88b935c3e587e3fab32401ebc
Joseph Jojoe <joseph.jojoe101@gmail.com>
2021-08-11 16:06:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ for i in range(0, len(labels)):
         labels[i][j] = int(labels[i][j])
 for i in range(0, len(labels)):
     labels[i] = np.array(labels[i])
-
     
 # Vectorises syllable strings by treating each character as a token
 tokenizer = tf.keras.preprocessing.text.Tokenizer(char_level=True)
@@ -38,3 +37,23 @@ padded_inputs = tf.keras.preprocessing.sequence.pad_sequences(
 padded_outputs = tf.keras.preprocessing.sequence.pad_sequences(
     labels, padding="post", maxlen=15
 )    
+
+# Normalisation
+maximum_token = 37
+for element in range(0, len(words)):
+    words[element] = words[element] / maximum_token
+
+# Shuffles data
+seed = random.random()
+random.seed(seed)
+random.shuffle(padded_inputs)
+random.seed(seed)
+random.shuffle(padded_outputs)
+
+# Splits into training, validation, and test sets (64-16-20 split)
+training_inputs = padded_inputs[0:113590]
+training_outputs = padded_outputs[0:113590]
+validation_inputs = padded_inputs[113590:141987]
+validation_outputs = padded_outputs[113590:141987]
+test_inputs = padded_inputs[141987:]
+test_outputs = padded_outputs[141987:]
