73f43ba3c0d5bcd58d79272835c5f2cd09bc6649
Joseph Jojoe <joseph.jojoe101@gmail.com>
2021-08-11 16:13:38 +0100

    Removed custom loss

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tensorflow_addons as tfa
 import pandas as pd
 import numpy as np
 import random
+from loss import mean_weighted_bce_mse
 
 
 # Loads in syllable data
@@ -57,25 +58,6 @@ validation_inputs = padded_inputs[113590:141987]
 validation_outputs = padded_outputs[113590:141987]
 test_inputs = padded_inputs[141987:]
 test_outputs = padded_outputs[141987:]
-
-# Custom loss function - mean of binary crossentropy and mean squared error
-def mean_weighted_bce_mse(y_true, y_prediction):
-    y_true = tf.cast(y_true, tf.float32)
-    y_prediction = tf.cast(y_prediction, tf.float32)
-
-    # Binary crossentropy with weighting
-    epsilon = 1e-6
-    positive_weight = 4.108897148948174
-    loss_positive = y_true * tf.math.log(y_prediction + epsilon)
-    loss_negative = (1 - y_true) * tf.math.log(1 - y_prediction + epsilon)
-    bce_loss = tf.math.reduce_mean(tf.math.negative(positive_weight * loss_positive + loss_negative))
-    
-    # Mean squared error
-    mse = tf.keras.losses.MeanSquaredError()
-    mse_loss = mse(y_true, y_prediction)
-
-    averaged_bce_mse = (bce_loss + mse_loss) / 2
-    return averaged_bce_mse
 
 # Builds syllabification model with Keras Functional API.
 inputs = tf.keras.Input(shape=(15,))
