c6efc166f96f1f91c4b818c7df56b880e61f16c0
Joseph Jojoe <joseph.jojoe101@gmail.com>
2021-08-11 17:26:43 +0100

    Added `BatchNormalization` layers

--- model.py
+++ model.py
@@ -4,29 +4,30 @@ from loss import mean_weighted_bce_mse
 
 
 def build_model():
-    # Builds syllabification model with Keras Functional API.
     inputs = tf.keras.Input(shape=(15,))
-    embedded_inputs = tf.keras.layers.Embedding(64, 64, mask_zero=True)(inputs)
-     
+    embedded_inputs = tf.keras.layers.Embedding(64, 128, mask_zero=True)(inputs)
+    embedded_inputs = tf.keras.layers.BatchNormalization()(embedded_inputs)
+
     x = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True, dropout=0.1, activity_regularizer=tf.keras.regularizers.l2(1e-5)))(embedded_inputs)
+    x = tf.keras.layers.concatenate([x, embedded_inputs])
+    x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True, dropout=0.1, activity_regularizer=tf.keras.regularizers.l2(1e-5)))(x)
+    x = tf.keras.layers.concatenate([x, embedded_inputs])
+    x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True, activity_regularizer=tf.keras.regularizers.l2(1e-5)))(x)
+    x = tf.keras.layers.BatchNormalization()(x)
 
-    y = tf.keras.layers.Conv1D(128, kernel_size=1, activation="tanh", activity_regularizer=tf.keras.regularizers.l2(1e-5))(embedded_inputs)
-    y = tf.keras.layers.Dropout(0.5)(y)
-    y = tf.keras.layers.Conv1D(128, kernel_size=1, activation="tanh", activity_regularizer=tf.keras.regularizers.l2(1e-5))(y)
-    y = tf.keras.layers.Dropout(0.5)(y)
-    y = tf.keras.layers.Conv1D(128, kernel_size=1, activation="tanh", activity_regularizer=tf.keras.regularizers.l2(1e-5))(y)
+    inception_output = inception_module(embedded_inputs)
 
-    merged_outputs = tf.keras.layers.concatenate([x, y])
-
-    x = tf.keras.layers.Dropout(0.5)(merged_outputs)
-    x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(128, activation="relu"))(x)
-    x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(64, activation="relu"))(x)
-    x = tf.keras.layers.GlobalMaxPool1D()(x)
-    x = tf.keras.layers.Dense(15, activation="sigmoid")(x)
-
-    model = tf.keras.models.Model(inputs=inputs, outputs=x)
+    output = tf.keras.layers.concatenate([inception_output, embedded_inputs])
+    output = tf.keras.layers.add([output, x])
+    output = tf.keras.layers.BatchNormalization()(output)
+    output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(256, activation="relu"))(output)
+    output = tf.keras.layers.BatchNormalization()(output)
+    output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(64, activation="relu"))(output)
+    output = tf.keras.layers.BatchNormalization()(output)
+    output = tf.keras.layers.GlobalMaxPool1D()(output)
+    output = tf.keras.layers.Dense(15, activation="sigmoid")(output)
 
     metrics = ["binary_accuracy",
                tfa.metrics.F1Score(num_classes=15, threshold=0.5),
@@ -35,6 +36,7 @@ def build_model():
                tf.keras.metrics.Precision(),
                tf.keras.metrics.AUC(multi_label=True, num_labels=15)]
 
+    model = tf.keras.models.Model(inputs=inputs, outputs=output)
     model.compile(optimizer="adam",
                   loss=mean_weighted_bce_mse,
                   metrics=metrics,
